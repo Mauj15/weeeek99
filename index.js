da4a0af6b16b0736c0d8d3e202e22e7080296759
Mauj15 <mauj1_15@uni.worc.ac.uk>
2016-12-01 10:50:06 +0000

    weere
    aras

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var watchID;
-var accelerometerOptions = { frequency: 2000 };  // Update every 2 seconds
-accelerometerOptions.frequency = 3000; //changed my mind - now 3 seconds
+var accelerometerOptions = { frequency: 100 };  // Update every 2 seconds
+accelerometerOptions.frequency = 100; //changed my mind - now 3 seconds
 
 
 //when the page is created...
